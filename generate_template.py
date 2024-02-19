230ae6e6efdf7a537dbd59f0a27d383cc318064e
jimmfan <jimmfan@users.noreply.github.com>
2024-02-18 19:21:46 -0600

    added dynamic output directory

--- generate_template.py
+++ generate_template.py
@@ -5,19 +5,17 @@ import subprocess
 def main():
     template_repo_url = os.getenv('TEMPLATE_REPO_URL')
     template_variables_json = os.getenv('TEMPLATE_VARIABLES_JSON')
+    template_directory = os.getenv('TEMPLATE_DIRECTORY', '.')  # Default to '.'
+    output_directory = os.getenv('OUTPUT_DIRECTORY', '.')  # Default to '.'
     template_variables = json.loads(template_variables_json)
 
-    # Call cookiecutter with the environment variables
-    subprocess.run(
-        [
-            'cookiecutter', 
-            '--no-input', 
-            '--output-dir', 
-            './generated',
-            template_repo_url
-        ], 
-        env={**os.environ, **template_variables}
-    )
+    command = ['cookiecutter', '--no-input', '--output-dir', output_directory]
+    if template_directory != '.':
+        command += ['--directory', template_directory]
+    command.append(template_repo_url)
+
+    # Execute the cookiecutter command with the specified environment variables
+    subprocess.run(command, env={**os.environ, **template_variables})
 
 if __name__ == "__main__":
     main()
