eacb0204db06b896d12222776a8525bd5308991a
jimmfan <jimmfan@users.noreply.github.com>
2024-02-19 17:30:20 -0600

    added debug print steps)

--- generate_template.py
+++ generate_template.py
@@ -9,6 +9,9 @@ template_repo_url = os.getenv('TEMPLATE_REPO_URL')
 template_directory = os.getenv('TEMPLATE_DIRECTORY', '.')  # Default to '.'
 output_directory = os.getenv('OUTPUT_DIRECTORY', '.')  # Default to '.'
 
+print(f"extra_context type: {type(extra_context)}")
+print(f"extra_context content: {extra_context}")
+
 # Call cookiecutter with the loaded context and optional template directory
 cookiecutter(
     template_repo_url,
