ea66028fc753bc6cb834d2ad003680cd37def036
jimmfan <jimmfan@users.noreply.github.com>
2024-02-27 03:32:58 +0000

    Updated action to use generate_template. updated devcontainer for basic_terraform

--- generate_template.py
+++ generate_template.py
@@ -1,21 +1,19 @@
 import os
 import json
-import sys
 from cookiecutter.main import cookiecutter
 
 # Load JSON context from a file
 with open(os.getenv('TEMPLATE_VAR_PATH'), 'r') as f:
     extra_context = json.load(f)
 
-template_repo_url = os.getenv('TEMPLATE_REPO_URL')
+template_repo_url = 'https://github.com/jimmfan/cookiecutter-action.git' 
 template_directory = os.getenv('TEMPLATE_DIRECTORY', '.')  # Default to '.'
-output_directory = os.getenv('OUTPUT_DIRECTORY', '.')  # Default to '.'
 
-# Call cookiecutter with the loaded context and optional template directory
+# Call cookiecutter with the loaded context and template directory
 cookiecutter(
     template_repo_url,
     no_input=True,
     extra_context=extra_context,
     directory=template_directory,
-    output_dir=output_directory
+    output_dir='.'
 )
