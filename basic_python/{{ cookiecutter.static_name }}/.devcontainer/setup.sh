714a93d3de7c1fa6b880d55520589f723015780e
jimmfan <jimmfan@users.noreply.github.com>
2025-02-21 14:23:10 -0600

    Added setup.sh for post create commands

---
+++ basic_python/{{ cookiecutter.static_name }}/.devcontainer/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e  # Exit if any command fails
+
+# Update and install dependencies
+sudo apt-get update
+sudo apt-get install -y tesseract-ocr mupdf mupdf-tools libmupdf-dev
+
+# Configure Git
+git config --global user.email "jimmfan@users.noreply.github.com"
+git config --global user.name "jimmfan"
+git config --global --add safe.directory "${containerWorkspaceFolder}"
